1341fb82edd22865cf18140189c6da738f7cb388
LCHKong <765353128@qq.com>
2020-04-29 11:55:13 +0800

    first test

---
+++ mylibrary/src/main/java/com/lch/mylibrary/TextUtils.java
@@ -0,0 +1,12 @@
+package com.lch.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by LCH on 2020/4/29 11:44.
+ */
+public class TextUtils {
+    public static void testLog() {
+        Log.d("LCH-->>", "test");
+    }
+}
